b0d6d6faa1e371e5ed06edda605639069c240114
Nicolas Goudry <goudry.nicolas@gmail.com>
2025-09-21 23:26:19 +0200

    feat(templates): make sure registration email has been touched

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,25 +1,53 @@
 // eslint-disable-next-line import-x/no-absolute-path
-import { registerIconLibrary } from '/assets/vendor/webawesome/webawesome.js'
+import { allDefined, registerIconLibrary } from '/assets/vendor/webawesome/webawesome.js'
 
-const getIconUrl = (name, family, variant) => {
-  let folder = 'solid'
+function getIconUrl (name, family, variant) {
+  let dir = 'solid'
 
   // Classic
   if (family === 'classic') {
-    if (variant === 'regular') folder = 'regular'
-    if (variant === 'solid') folder = 'solid'
+    if (variant === 'regular') dir = 'regular'
+    if (variant === 'solid') dir = 'solid'
   }
 
   // Brands
   if (family === 'brands') {
-    folder = 'brands'
+    dir = 'brands'
   }
 
-  return `/assets/vendor/fontawesome/svg/${folder}/${name}.svg`
+  return `/assets/vendor/fontawesome/svg/${dir}/${name}.svg`
 }
 
-registerIconLibrary('default', {
-  resolver (name, family = 'classic', variant = 'solid') {
-    return getIconUrl(name, family, variant)
-  },
+async function validateRegisterForm () {
+  const registerForm = document.getElementById('register')
+
+  if (!registerForm) {
+    return
+  }
+
+  const emailInput = registerForm.querySelector('wa-input')
+  const realEmailInput = emailInput.shadowRoot.querySelector('input[name="email"]')
+  const errorDialog = document.getElementById('error-dialog')
+
+  registerForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    if (realEmailInput.value === emailInput.defaultValue) {
+      errorDialog.open = true
+
+      return
+    }
+
+    e.target.submit()
+  })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  registerIconLibrary('default', {
+    resolver (name, family = 'classic', variant = 'solid') {
+      return getIconUrl(name, family, variant)
+    },
+  })
+
+  allDefined().then(validateRegisterForm)
 })
